883177655d08ddbb3d436a508e96709cdaec0cc5
emerzse <emerzse@gmail.com>
2016-04-07 08:59:31 +0000

    Config tutorial important objects

---
+++ extracting-conserver/src/main/java/msvcdojo/ConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package msvcdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigurationServerApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ConfigurationServerApplication.class, args);
+    }
+}
